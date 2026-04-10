90ddea3ecac7a38ac75c3e5823f412c099639d28
Pachix-Dev <pachi.claros@gmail.com>
2026-04-10 17:30:13 -0600

    feat: agregar logo en la página de inicio de sesión y mejorar el diseño del formulario

--- apps/gkx-web-app/app/(public)/login/page.tsx
+++ apps/gkx-web-app/app/(public)/login/page.tsx
@@ -8,6 +8,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useAuth } from "@/features/auth/use-auth";
 import { LoginFormValues, loginSchema } from "@/features/auth/schemas/self-service";
 import { sileo } from "sileo";
+import Image from "next/image";
 
 function LoginPageContent() {
   const router = useRouter();
@@ -82,60 +83,60 @@ function LoginPageContent() {
 
   return (
     <div className="grid min-h-screen bg-muted p-6 md:place-items-center">
-      <div className="mx-auto w-full max-w-md rounded-2xl border border-border bg-card p-7 shadow-sm">
-        <div className="text-center">
-            <p className="text-xs uppercase tracking-[0.2em] text-muted-foreground">GKX</p>
-            <h1 className="mt-2 text-2xl font-semibold text-card-foreground">Iniciar sesión</h1>
-            <div className="flex items-center justify-center gap-1">
-                <p className="text-sm text-muted-foreground">
-                    ¿No tienes una cuenta? 
-                </p>
-                <Link href="/register" className="text-foreground text-sm font-bold hover:text-primary">
-                    Regístrate aquí
-                </Link>
-            </div>
+      <div className="mx-auto w-full max-w-xl ">
+        <Image src="/logo_gkx_white.webp" alt="GKX Logo" width={96} height={52} className="mx-auto rounded-2xl border border-border p-2" />
+        <div className="mt-2 rounded-2xl border border-border bg-card p-7 shadow-sm">
+          <h1 className="mt-2 text-2xl font-semibold text-card-foreground">Iniciar sesión</h1>
+          <div className="flex items-center justify-center gap-1">
+              <p className="text-sm text-muted-foreground">
+                  ¿No tienes una cuenta? 
+              </p>
+              <Link href="/register" className="text-foreground text-sm font-bold hover:text-primary">
+                  Regístrate aquí
+              </Link>
+          </div>
+          <form className="mt-6 space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+            <label className="block space-y-1">
+              <span className="text-sm text-foreground">Email</span>
+              <input
+                type="email"
+                required
+                {...form.register("email")}
+                className="w-full rounded-md border border-border bg-background px-3 py-2 text-foreground outline-none ring-offset-2 transition placeholder:text-muted-foreground focus:ring-2 focus:ring-primary"
+                placeholder="coach@gkacademy.com"
+              />
+              {form.formState.errors.email ? <p className="text-xs text-red-600">{form.formState.errors.email.message}</p> : null}
+            </label>
+
+            <label className="block space-y-1">
+              <div className="flex justify-between">
+                  <span className="text-sm text-foreground">Contraseña</span>
+                  <Link href="/forgot-password" className="ml-1 text-sm text-primary hover:text-primary/80">
+                      ¿Olvidaste tu contraseña?
+                  </Link>
+              </div>
+              <input
+                type="password"
+                required
+                {...form.register("password")}
+                className="w-full rounded-md border border-border bg-background px-3 py-2 text-foreground outline-none ring-offset-2 transition placeholder:text-muted-foreground focus:ring-2 focus:ring-primary"
+                placeholder="********"
+              />
+              {form.formState.errors.password ? <p className="text-xs text-red-600">{form.formState.errors.password.message}</p> : null}
+            </label>
+
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="w-full rounded-md bg-primary px-3 py-2 text-sm font-medium text-primary-foreground transition hover:bg-secondary hover:text-secondary-foreground disabled:opacity-60"
+            >
+              {isSubmitting ? "Entrando..." : "Entrar"}
+            </button>
+            
+              <p className="text-xs text-center">Al iniciar sesión, acepta nuestros Términos y Política de privacidad.</p>                       
+            
+          </form>
         </div>
-        <form className="mt-6 space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-          <label className="block space-y-1">
-            <span className="text-sm text-foreground">Email</span>
-            <input
-              type="email"
-              required
-              {...form.register("email")}
-              className="w-full rounded-md border border-border bg-background px-3 py-2 text-foreground outline-none ring-offset-2 transition placeholder:text-muted-foreground focus:ring-2 focus:ring-primary"
-              placeholder="coach@gkacademy.com"
-            />
-            {form.formState.errors.email ? <p className="text-xs text-red-600">{form.formState.errors.email.message}</p> : null}
-          </label>
-
-          <label className="block space-y-1">
-            <div className="flex justify-between">
-                <span className="text-sm text-foreground">Contraseña</span>
-                <Link href="/forgot-password" className="ml-1 text-sm text-primary hover:text-primary/80">
-                    ¿Olvidaste tu contraseña?
-                </Link>
-            </div>
-            <input
-              type="password"
-              required
-              {...form.register("password")}
-              className="w-full rounded-md border border-border bg-background px-3 py-2 text-foreground outline-none ring-offset-2 transition placeholder:text-muted-foreground focus:ring-2 focus:ring-primary"
-              placeholder="********"
-            />
-            {form.formState.errors.password ? <p className="text-xs text-red-600">{form.formState.errors.password.message}</p> : null}
-          </label>
-
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className="w-full rounded-md bg-primary px-3 py-2 text-sm font-medium text-primary-foreground transition hover:bg-secondary hover:text-secondary-foreground disabled:opacity-60"
-          >
-            {isSubmitting ? "Entrando..." : "Entrar"}
-          </button>
-          
-            <p className="text-xs text-center">Al iniciar sesión, acepta nuestros Términos y Política de privacidad.</p>                       
-          
-        </form>
       </div>
     </div>
   );
